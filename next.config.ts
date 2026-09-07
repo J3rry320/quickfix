70e74007ce7c95f03907b99fd70bbb07f8eaa8a6
J3rry320 <rutu.satpathy44@gmail.com>
2026-09-07 16:57:46 +0530

    Remember to add firebase-admin to it's own package everytime mate!

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["firebase-admin"],
   images: {
     remotePatterns: [
       {
