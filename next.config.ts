c807a1640ef6ac7b7b21558d846dd5107b1d0a54
J3rry320 <rutu.satpathy44@gmail.com>
2026-09-07 17:26:33 +0530

    Does updating the package make it work? Let's see

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,14 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["firebase-admin"],
+  // serverExternalPackages: ["firebase-admin"],
+  compress: true,
+  poweredByHeader: false,
   images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 31536000,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         hostname: "i.ibb.co",
